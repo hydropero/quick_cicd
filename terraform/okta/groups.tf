ee5c4572bc38a558d651ca4457e9df97be4452c9
Myles Bulla <mylesjbulla@gmail.com>
2025-11-25 00:31:46 -0600

    adding additional okta group for new department

--- terraform/okta/groups.tf
+++ terraform/okta/groups.tf
@@ -8,3 +8,8 @@ resource "okta_group" "operations" {
   name        = "ops"
   description = "Ops team"
 }
+
+resource "okta_group" "systems_administration" {
+  name        = "sysadmin"
+  description = "Sysadmin team"
+}
